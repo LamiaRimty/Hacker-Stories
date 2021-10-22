736b86e6a42cfc3bc787b7f27a69c1f818a6c412
Lamia Rimty <rimtycse@gmail.com>
2021-10-22 20:39:29 +0600

    Component & JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+function getTitile(title){
+  return title;
+}
+
 function App() {
-  const title ='React';
+ 
+  
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1> Hello {getTitile('React')} </h1>
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
