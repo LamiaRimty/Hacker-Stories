798c0d79d4f70076abd4fa5b0d99fe3ebc33e9e8
Lamia Rimty <rimtycse@gmail.com>
2021-10-25 23:45:57 +0600

    lists

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React from 'react';
 
-function getTitile(title){
-  return title;
-}
-
-function App() {
- 
+const list = [
   
-  return (
-    <div>
-      <h1> Hello {getTitile('React')} </h1>
-      <label htmlFor="search"> Search: </label>
-      <input id="search" type="text"/>
-    </div>
-  );
+  {
+    title:'React',
+    url: 'https://reactjs.org/',
+    author : "Jordan Walke",
+    num_commnets: 3,
+    points : 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redus.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_commnets: 2,
+    points: 5,
+    objectID: 1,
+},
+];
+
+function App(){
+ return (
+   <div>
+     <h1>My Hacker Stories</h1>
+    <label htmlFor="search" >Search</label>
+    <input id="search" type="text"/>
+
+  <hr/>
+{ list.map(function(item){
+  return <div key={item.objectID}>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_commnets}</span>
+    <span>{item.points}</span>
+
+
+  </div>;
+
+})}
+   </div>
+ );
+
 }
 
 export default App;
