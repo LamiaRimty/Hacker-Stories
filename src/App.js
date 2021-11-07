913bf39c8647aab39dd2be8e4e73b449fb2d3ab6
Lamia Rimty <rimtycse@gmail.com>
2021-11-07 16:43:18 +0600

    props

--- src/App.js
+++ src/App.js
@@ -1,85 +1,52 @@
 import React from 'react';
 
+const App=()=>{
 
-const list = [
+  const stories = [
   
-  {
-    title:'React',
-    url: 'https://reactjs.org/',
-    author : "Jordan Walke",
-    num_commnets: 3,
-    points : 4,
-    objectID: 0,
+    {
+      title:'React',
+      url: 'https://reactjs.org/',
+      author : "Jordan Walke",
+      num_commnets: 3,
+      points : 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redus.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_commnets: 2,
+      points: 5,
+      objectID: 1,
   },
-  {
-    title: 'Redux',
-    url: 'https://redus.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_commnets: 2,
-    points: 5,
-    objectID: 1,
-},
-];
+  ];
 
-function App(){
- return (
-   <div>
-     
-     <h1>My Hacker Stories</h1>
-    <label htmlFor="search" >Search</label>
-    <input id="search" type="text"/>
+  const handleChange=(event)=>{
+    console.log(event.target.value);
+  };
 
-    <hr/>
-  <List/> 
-  
-</div>
-
- );
-
-}
-
-function List() {
-  return (
-
-    <ul>
-    {list.map(function(item) //returns a new array containing the results of calling a function on every element in this array
-    {  
-     return( 
-       <li key={item.objectID}>
-       <span>
-        <a href={item.url}>{item.title}</a>
-       </span>
-       <span>{item.author}</span>
-       <span>{item.num_commnets}</span>
-       <span>{item.points}</span>
-
-
-      </li>
-     );
-    })}
- </ul>
-
-  );
-}
-
-
-const App=()=>(
- 
+ return(
     <div>
       <h1>My Hacker Stories</h1>
 
     <label htmlFor="search" >Search: </label>
-    <input id="search" type="text" ></input>
+    <input
+     id="search"
+     type="text" 
+     onChange={handleChange} />
     <hr/>
-    <List/>
+    <List list={ stories} />
     </div>
+ );
+ };
   
-);
+  const List=(props)=>(
   
-  const list=()=>
-     list.map(item =>(
+    <ul>
+      {props.list.map((item) =>(
      
-        <div key={item.objectID}>
+        <li key={item.objectID}>
           <span>
             <a href={item.url}>{ item.title}</a>
           </span>
@@ -87,8 +54,10 @@ const App=()=>(
           <span>{item.num_commnets}</span>
           <span>{item.points}</span>
   
-        </div>
-     ));
+        </li>
+      ))}
+    </ul>
+  );
  
   
 
