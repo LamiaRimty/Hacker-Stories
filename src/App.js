9372452d53a3db2ede0ce492d0e34d4edf417a95
Lamia Rimty <rimtycse@gmail.com>
2022-01-03 12:12:27 +0600

    SVGs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { ReactComponent as Check } from './check.svg';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query= ';
 
@@ -67,9 +68,90 @@ const storiesReducer = (state, action) => {
   }
 };
 
+/*
+const StyledContainer =styled.div`
 
+height:100vw;
+padding:20px;
+background:#83a4d4;
+background: linear-gradient(to left,b6fbff,#83a4d4)
+color: #17121;
+`;
 
+const StyledHeadlinePrimary=styled.h1`
 
+font-size:48px;
+font-weight-300px;
+letter-spacing:2px;
+`;
+
+const StyledItem=styled.div`
+display:flex;
+align-items:center;
+padding-bottom: 5px;
+`;
+
+const StyledColumn=styled.span`
+padding:0 5px;
+white-space:nowrap;
+overflow:hidden;
+white-space:nowrap;
+text-overflow:ellipsis;
+
+a{
+  color:inherit;
+}
+
+width: ${props=>props.width}
+
+`;
+
+const StyledButton=styled.button`
+
+background:transparent;
+border:1px solid #171212;
+padding:5px;
+cursor:pointer;
+
+transition:all 0.1s ease-in;
+
+&:hover{
+  background: #171212;
+  color:#ffffff;
+}
+`;
+
+const StyledButtonSmall=styled(StyledButton)`
+padding: 5px;
+`;
+
+const StyledButtonLarge=styled(StyledButton)`
+
+padding 10px;
+`;
+
+const StyledSearchForm=styled.form`
+padding:10px 0 20px 0;
+display:flex;
+align-items:baseline;
+`;
+
+const StyledLabel=styled.label`
+border-top: 1px solid #171212;
+border-left: 1px solid #171212;
+padding-left: 5px;
+font-size:24px;
+
+`;
+
+const StyledInput= styled.input `
+border:none;
+border-bottom: 1px solid #171212;
+background-color: transparent;
+font-size:24px;
+`;
+
+*/
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
@@ -124,7 +206,7 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1 className="headline-primary">My Hacker Stories</h1>
+     <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -158,8 +240,7 @@ const SearchForm = ({
 
   <form
    onSubmit= {onSearchSubmit}
-   className="search-form"
-   >
+   className='search-form' >
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -173,7 +254,7 @@ const SearchForm = ({
     <button
       type="submit"
       disabled={!searchTerm}
-      className='button button-large'
+      className="button button_large"
     >
       Submit
     </button>
@@ -235,20 +316,22 @@ const Item = ({ item, onRemoveItem }) => (
 
 
   <div className="item">
-    <span style={{width:"40%"}}>
+    <span style={{width:'40%'}} >
 
       <a href={item.url}>{item.title}</a>
     </span>
-    <span style={{width:"30%"}}>{item.author}</span>
-    <span style={{width:"10%"}}>{item.num_commnets}</span>
-    <span style={{width:"10%"}}>{item.points}</span>
-    <span style={{width:"10%"}}>
-      <button
-      className='button button-small'
+
+    <span style={{width:'30%'}} >{item.author}</span>
+    <span style={{width:'10%'}} >{item.num_commnets}</span>
+    <span style={{width:'10%'}} >{item.points}</span>
+    <span style={{width:'10%'}} >
+    
+         <button
         type="button"
         onClick={() => onRemoveItem(item)}
+        className="button button_small"
       >
-        Dismiss
+     <Check height="18px" width="18px"/>
       </button>
     </span>
   </div>
